374e931683c6c34e537e6ba7ebace570174a8a87
ricky gould <rgould2021@fau.edu>
2025-01-25 15:13:24 -0500

    added .gitignore and .env

--- LeagueAPI.py
+++ LeagueAPI.py
@@ -1,14 +1,18 @@
 #I want the program to take my last 5 matches into account and recommmend a player based off win rate or stats prior.
 import requests
-API_KEY = "RGAPI-b61d3bf1-a2f7-44af-8b62-4366bd2ff63b"
-headers = {
+import os
+from dotenv import load_dotenv, dotenv_values 
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+
+params = {
     "X-Riot-Token": API_KEY
 }
 def calculateChamp():
-    gameName=input("Enter game name: ")
-    tagLine=input("Enter the tagline #: ")
-    url = "https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{gameName}/{tagLine}"
-    response = requests.get(url, headers=headers)
+    gameName=input("Enter Game Name: ")
+    tagLine=input("Enter the Tagline #: ")
+    url = "https://americas.api.riotgames.com/riot/?api_key={API_KEY}"
+    response = requests.get(url, params=params)
     print(response)
     #MaCnNaP#9601
 calculateChamp()
